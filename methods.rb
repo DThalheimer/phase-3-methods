4be7ec33f69801d124128399233bf99d54cbd6bf
Andrew Thalheimer <drewt141992@yahoo.com>
2022-11-23 14:31:11 -0500

    commit tree d3f57fce0f645f56df4d8979eda53f8597cc0dce

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+
+
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+
+
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+
+
+
+
+def add(num1, num2)
+  num1 + num2
+end
+
+
+def halve(number)
+  if number = number.to_s
+   return nil
+  elsif
+  
+ number/2
+end
+end
